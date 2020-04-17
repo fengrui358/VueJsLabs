58efc6cd805034a552c01322081211adfcf30298
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-04-17 17:04:32 +0800

    临时提交

--- demo01/js/index.js
+++ demo01/js/index.js
@@ -1,3 +1,41 @@
-function down() {
-  console.log("sa");
-}
+var styleObj = {
+  "text-decoration": "line-through",
+  color: "red",
+};
+
+var vm = new Vue({
+  el: "#app",
+  data: {
+    message: "hello",
+    value: "ghost",
+    item: {
+      title: "课程1",
+      del: false,
+    },
+    list: [
+      {
+        title: "课程1",
+        del: false,
+      },
+      {
+        title: "课程2",
+        del: true,
+      },
+      {
+        title: "课程3",
+        del: false,
+      },
+    ],
+    styleObj: styleObj,
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+    deleteHandler: function (title, event) {
+
+    }
+  },
+});
+
+vm.styleObj.color = "blue";
